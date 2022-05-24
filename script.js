320d2cebb0844a2b25fa3fb2e8633ce853a79361
Augusto Deolindo <101934746+AugustoADeolindo@users.noreply.github.com>
2022-05-23 22:00:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
+var tarefas = ["Augusto"];
+
+function listarTodasTarefas(){
+    var lista = document.getElementById("lista")
+    lista.innerHTML = "";
+    var modelo = document.querySelector("#modelo");
+
+
+    for (var i = 0; i< tarefas.length; i++){
+
+        var novo = modelo.content.cloneNode(true);
+        var li = novo.querySelector("li");
+        li.append(tarefas[i]);
+
+        lista.append(novo);
+    }
+}
+
 
 function adicionarElementos(){
-    var modelo = document.getElementById("modelo");
-    var novo = modelo.content.cloneNode(true);
-
-    var li = novo.querySelector("li");
-    var item = document.getElementById("textoDoUsuario")
-    var p = novo.querySelector("p");
-    p.innerHTML = item.value;
     
+    var item = document.getElementById("textoDoUsuario").value
+    tarefas.push(item);
+    var p = document.querySelector("p")
+    p.innerHTML = item;
+
+    listarTodasTarefas();
 
 
-    document.getElementById("lista").append(li);
 }
+
 function remover(){
-    lista.innerHTML = '';
+    area.innerHTML = '';
 }
 
