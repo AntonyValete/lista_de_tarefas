b499e8d2b668b913853aa26dd1a1f583c2688f7e
Antony Valete <antonyvalete@gmail.com>
2022-07-14 01:47:45 -0300

    fix: modificando esqueleto e textos

--- demo/script.js
+++
@@ -1,7 +0,0 @@
-function add() {
-    var texto = document.getElementById("campoDeTexto");
-    var itemDaLista = document.createElement("li")
-    itemDaLista.innerHTML = texto.value
-    document.getElementById("lista").append(itemDaLista)
-    texto.value = ""
-}
